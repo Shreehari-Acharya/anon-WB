4c7fbc868c521fa99b2d08a3ffd1c984f23e3bb5
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2024-08-31 09:58:11 +0530

    And some basic funtionality on backend.

--- server.js
+++ server.js
@@ -1,2 +1,61 @@
 var DiamSdk = require("diamnet-sdk");
+const express = require('express')
 
+const app = express()
+const port = 3000
+
+import { PinataSDK } from "pinata";
+
+const pinata = new PinataSDK({
+  pinataJwt: "PINATA_JWT",
+  pinataGateway: "example-gateway.mypinata.cloud",
+});
+
+
+app.use(express.static('public'));
+
+
+async function uploadFileToIPFS(file) {
+  try {
+    const upload = await pinata.upload.file(file);
+    console.log(upload);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getFileFromIPFS(hash) {
+  try {
+    const data = await pinata.gateways.get(hash);
+    console.log(data)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.post('/upload', (req, res) => {
+  const file = req.body.file;
+  const publicKey = req.body.publicKey;
+  uploadFileToIPFS(file).then((data) => {
+    const cid = data.IpfsHash;
+    // update database with public key and cid
+    res.send({cid: cid});
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
